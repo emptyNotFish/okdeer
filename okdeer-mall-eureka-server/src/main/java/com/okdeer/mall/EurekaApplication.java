88ce4b7e086d7f7a77c30ec8a6ae583a53c795d8
emptyNotFish <xu13727049636@sina.com>
2018-03-29 18:46:45 +0800

    增加配置信息

--- okdeer-mall-eureka-server/src/main/java/com/okdeer/mall/EurekaApplication.java
+++ okdeer-mall-eureka-server/src/main/java/com/okdeer/mall/EurekaApplication.java
@@ -1,8 +1,15 @@
 package com.okdeer.mall;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 
 /**
  * Created by pact on 2018/3/28.
@@ -11,7 +18,21 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class EurekaApplication {
 
-    public static void main(String[] args){
-        SpringApplication.run(EurekaApplication.class, args);
+    private static final Logger LOGGER = LoggerFactory.getLogger(EurekaApplication.class);
+
+    public static void main(String[] args) throws UnknownHostException {
+        SpringApplication application = new SpringApplication(EurekaApplication.class);
+        Environment environment = application.run(args).getEnvironment();
+
+        LOGGER.info("\n----------------------------------------------------------\n\t" +
+                        "Application '{}' is running! Access URLs:\n\t" +
+                        "Local: \t\thttp://localhost:{}\n\t" +
+                        "External: \thttp://{}:{}\n\t" +
+                        "Profile(s): \t{}\n----------------------------------------------------------",
+                environment.getProperty("spring.application.name"),
+                environment.getProperty("server.port"),
+                InetAddress.getLocalHost().getHostAddress(),
+                environment.getProperty("server.port"),
+                environment.getActiveProfiles());
     }
 }
--- okdeer-mall-web-service/src/main/java/com/okdeer/mall/WebServiceApplication.java
+++ okdeer-mall-web-service/src/main/java/com/okdeer/mall/WebServiceApplication.java
@@ -2,12 +2,15 @@ package com.okdeer.mall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * 后台具体实现项目入口
  * Created by pact on 2018/3/28.
  */
 @SpringBootApplication
+@EnableEurekaClient // 非eureka做注册中心用@EnableDiscoveryClient 其包含EnableEurekaClient功能，eureka做注册中心 建议用EnableEurekaClient
 public class WebServiceApplication{
 
     public static void main(String[] args){
